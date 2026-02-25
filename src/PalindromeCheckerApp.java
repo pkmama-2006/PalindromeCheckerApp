f4bf78f4b47a4f4271fc74e72d1e469e94adbdb1
pkmama-2006 <vg6285@srmist.edu.in>
2026-02-25 15:29:19 +0530

    Base application setup (UC0)

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,13 @@
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("     PALINDROME CHECKER APP      ");
+        System.out.println("     Version 1.0                 ");
+        System.out.println("=================================");
+
+        System.out.println("Application Started Successfully!");
+
+    }
+}
