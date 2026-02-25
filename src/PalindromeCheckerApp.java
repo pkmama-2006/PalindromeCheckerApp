6f9e924822619ca2d0caa3c34166d55714f89796
pkmama-2006 <vg6285@srmist.edu.in>
2026-02-25 15:36:05 +0530

    UC2: Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -8,6 +8,20 @@ public class PalindromeCheckerApp {
         System.out.println("=================================");
 
         System.out.println("Application Started Successfully!");
+        String word = "madam";
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < word.length() / 2; i++) {
+            if (word.charAt(i) != word.charAt(word.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome)
+            System.out.println(word + " is a Palindrome");
+        else
+            System.out.println(word + " is NOT a Palindrome");
 
     }
 }
